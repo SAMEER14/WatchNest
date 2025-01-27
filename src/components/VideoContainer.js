82921d8d47904acf2f93cb30928390b23a6b8dbb
Sameer Sharma <sameersharma0140@gmail.com>
2025-01-27 21:19:25 +0530

    removed console

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -17,7 +17,7 @@ useEffect(() => {
 const getVideos = async() =>{
   const data = await fetch(YOUTUBE_VIDEO_API);
   const json = await data.json();
-  console.log(json.items);
+  // console.log(json.items);
   setVideos(json.items)
 }
 
